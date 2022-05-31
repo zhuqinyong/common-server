b4a460022e0c1468f885c799254197f9bacc389b
zhuqinyong <18326657779@163.com>
2022-05-31 11:05:05 +0800

    Initial commit

---
+++ src/test/java/me/tsinyong/common/server/CommonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tsinyong.common.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
