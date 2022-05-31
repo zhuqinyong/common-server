218201fc10472aa469143870268506a5d8a0ee22
zhuqinyong <18326657779@163.com>
2022-05-31 13:56:13 +0800

    提供者提交

---
+++ common-provider/src/main/java/me/tsinyong/commonprovider/CommonProviderApplication.java
@@ -0,0 +1,13 @@
+package me.tsinyong.commonprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonProviderApplication.class, args);
+    }
+
+}
--- common-provider/src/main/java/me/tsinyong/commonprovider/controller/CommonController.java
+++ common-provider/src/main/java/me/tsinyong/commonprovider/controller/CommonController.java
@@ -1,4 +1,4 @@
-package me.tsinyong.common.server.controller;
+package me.tsinyong.commonprovider.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
